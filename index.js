530b2735d003b02eeebd5550793055028dc11aee
Afrikanochka <Mtgnatali@gmail.com>
2021-06-29 18:06:37 +0300

    add class CountdownTimer

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-import './styles.css';
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.intervalId = null;
+    start();
+  }
+  start() {
+    this.intervalId = setInterval(() => {
+      const time = this.targetDate - Date.now();
+      if (time <= 0) {
+        clearInterval(this.intervalId);
+        return false;
+      }
+      const timeComponent = this.getTimeComponents(time);
+      this.onTimeTick(timeComponent);
+    }, 1000);
+  }
+}
+function onTimeTick({ days, hours, mins, secs }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.mins.textContent = mins;
+  refs.secs.textContent = secs;
+}
+// const timer = new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('Jul 17, 2019'),
+// });
+
+function getTimeComponents(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+  return { days, hours, mins, secs };
+}
